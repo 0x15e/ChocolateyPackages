3f9d89d1d98e8f29a7e7977b12ef19d06664af12
0x15e <thomas@vinylfetish.net>
2015-12-03 13:00:08 -0600

    added _output to .gitignore. removed uninstall script from snaketail because it can't work consistently. updated snaketail to work with ketarin as an auto package.

--- snaketail/tools/chocolateyInstall.ps1
+++ snaketail/tools/chocolateyInstall.ps1
@@ -1,17 +1,9 @@
-﻿#Changes per version
-$uninstallKey = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{60F454D1-1981-4721-B29E-5CF45A6CECCA}'
-
-$packageName = 'snaketail'
+﻿$packageName = 'snaketail'
 $installerType = 'msi'
-$url = 'https://snaketail-net.googlecode.com/files/SnakeTail.v1.8.2.x86.msi'
-$url64 = 'https://snaketail-net.googlecode.com/files/SnakeTail.v1.8.2.x64.msi'
+$url = '{{DownloadUrl}}'
+$url64 = 'https://github.com/{{DownloadUrlx64}}'
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
 
 
-if (Test-Path $uninstallKey) {
-  Write-ChocolateyFailure 'SnakeTail is already installed!'
-}
-else {
-  Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
-}
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
--- snaketail/tools/chocolateyUninstall.ps1
+++
@@ -1,2 +0,0 @@
-﻿$msiId = '{60F454D1-1981-4721-B29E-5CF45A6CECCA}'
-Uninstall-ChocolateyPackage 'snaketail' 'msi' "$msiId /qb" -validExitCodes @(0)
